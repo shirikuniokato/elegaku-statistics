273d8b3430a5ce4377adef64d5162da9be05dd6f
rikuto110511 <rikuto.511@docomo.ne.jp>
2022-12-22 18:54:36 +0900

    lambdaの新規追加

---
+++ 03_backend/batch/update-attendance-information-month/index.mjs
@@ -0,0 +1,28 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const aws = require('aws-sdk');
+aws.config.update({ region: 'ap-northeast-1' });
+const dynamo = new aws.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+
+export const handler = async (event) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  try {
+    body = await dynamo.scan({ TableName: 'attendance-information' }).promise();
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
