7f2516b46d87d8bec9814239b4092428d4354d5c
rikuto110511 <rikuto.511@docomo.ne.jp>
2023-01-04 07:51:53 +0900

    口コミのLambda作成完了

--- 03_backend/web-api/web-review/index.mjs
+++ 03_backend/web-api/web-review/index.mjs
@@ -11,17 +11,21 @@ export const handler = async (event) => {
   };
   let body = {
     review: {
-      isError: false,
-      errorMessage: '',
       count: 0,
       items: [],
     },
+    girl: {
+      count: 0,
+      items: [],
+    },
+    isError: false,
+    errorMessage: '',
   };
 
   try {
     switch (event.routeKey) {
-      case 'GET /review':
-        body = await getReview(body);
+      case 'GET /review/init':
+        body = await init(body);
         break;
       case 'POST /review/add':
         body = await putNotification(event);
@@ -43,17 +47,28 @@ export const handler = async (event) => {
 };
 
 // GET /review
-const getReview = async (body) => {
-  const result = await dynamo.scan({ TableName: 'review' }).promise();
+const init = async (body) => {
+  try {
+    const review = await dynamo.scan({ TableName: 'review' }).promise();
+    const girl = await dynamo.scan({ TableName: 'girl' }).promise();
 
-  body.review.items = result.Items;
-  body.review.count = result.Count;
+    // レビュー一覧の設定
+    body.review.items = review.Items;
+    body.review.count = review.Count;
+
+    // girlの設定
+    body.girl.items = girl.Items;
+    body.girl.count = girl.Count;
+  } catch (err) {
+    body.isError = true;
+    body.errorMessage = err.message;
+  }
+
   return body;
 };
 
 // POST /review/add
 const putNotification = async (event) => {
-  console.log(event);
   // postされたデータを取得
   const data = JSON.parse(event.body);
   // timestampをミリ秒で取得・設定
